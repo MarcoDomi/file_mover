2db2c2005cc9449e64f71d2069538530129b2047
Marco Dominguez <47532404+MarcoDomi@users.noreply.github.com>
2024-07-17 17:07:32 -0500

    check if file extension is valid

--- move.py
+++ move.py
@@ -1,7 +1,28 @@
 import os
+import shutil
+from datetime import datetime
+# TODO remove testfolder directory later
 
-entries = os.scandir("/mnt/c/Users/MarcoDominguez/Downloads")
+valid_extensions = [".jpg", ".jpeg", ".png"]
+source_path = "/mnt/c/Users/MarcoDominguez/Downloads"
+dest_path = "/home/marcodom/repos/file_mover/testfolder" #will change later
 
+def return_files(): #will delete later
+    with os.scandir(dest_path) as entries:
+        for entry in entries:
+            shutil.move(entry, source_path)
 
-for entry in entries:
-    print(entry.name)
+def is_valid_extension(file_name):
+    index = file_name.find('.')
+    file_extension = file_name[index:]
+    is_valid = file_extension in valid_extensions
+    return is_valid
+
+def file_mover():
+    with os.scandir(source_path) as entries:
+        for entry in entries:
+            if is_valid_extension(entry.name):
+                shutil.move(entry, dest_path)
+
+#return_files()
+file_mover()
