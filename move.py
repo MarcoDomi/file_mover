67ee6dcf11d4f7c1ba048db178f6f42ec93b8a32
Marco Dominguez <47532404+MarcoDomi@users.noreply.github.com>
2024-08-09 15:24:17 -0500

    add comments for clarification

--- move.py
+++ move.py
@@ -6,15 +6,15 @@ from watchdog.observers.polling import PollingObserver
 from watchdog.events import FileSystemEventHandler
 
 
-valid_extensions = [".jpg", ".jpeg", ".png"]
+valid_extensions = [".jpg", ".jpeg", ".png", ".json"]
 source_path = "/mnt/c/Users/MarcoDominguez/Downloads"
 
-if len(sys.argv) == 1:
-    dest_path = "/home/marcodom/Downloads"
+if len(sys.argv) == 1: 
+    dest_path = "/home/marcodom/Downloads" #if no dest_path is provided in terminal set dest_path to this directory
 else:
     dest_path = sys.argv[1]
 
-def return_files(): #will delete later
+def return_files(): #used to return files from dest_path back to source_path
     with os.scandir(dest_path) as entries:
         for entry in entries:
             shutil.move(entry, source_path)
@@ -33,12 +33,14 @@ def is_valid_extension(file_name):
     is_valid = file_extension in valid_extensions
     return is_valid
 
-def file_mover():
+
+def file_mover():  # if file has a valid extension and the modified time was no more than 10 minutes ago then move file to dest_path
     valid_min_elapsed = 10.0 #10 minutes
     with os.scandir(source_path) as entries:
         for entry in entries:
-            if is_valid_extension(entry.name) and min_since_file_modified(source_path + '/' + entry.name) < valid_min_elapsed:
+            if is_valid_extension(entry.name) and min_since_file_modified(source_path + '/' + entry.name) < valid_min_elapsed: 
                 shutil.move(entry, dest_path)
+
 
 class MyHandler(FileSystemEventHandler):
     def on_modified(self, event):
@@ -54,7 +56,7 @@ class MyHandler(FileSystemEventHandler):
 
 if __name__ == "__main__":
     event_handler = MyHandler()
-    observer = PollingObserver()
+    observer = PollingObserver() #use polling observer b/c source_path is on windows and dest_path is on wsl
     observer.schedule(event_handler, path=source_path, recursive=True)
     observer.start()
 
